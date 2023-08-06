8c89ad3c0a3e3b6e0b4068018d9c2d3b9153a99c
Artem Fadeev <126018223+fadeev19190@users.noreply.github.com>
2023-08-06 19:14:33 +0200

    Create Introduction to Randon Strings.py

---
+++ Introduction to Randon Strings/Introduction to Randon Strings.py
@@ -0,0 +1,28 @@
+import math
+
+with open('rosalind_prob.txt') as f:
+    s = f.readline().replace('\n', '')
+    a = f.readline().replace('\n', '').split(' ')
+
+gc_content = []
+for gc in a:
+    gc_int = float(gc)
+    gc_content.append(gc_int)
+
+prob_list = []
+for i in range(0, len(a)):
+    prob = 1
+    for nuc in s:
+        if nuc in ['C', 'G']:
+            b = gc_content[i] / 2
+            prob *= b
+        elif nuc in ['A', 'T']:
+            d = (1 - gc_content[i]) / 2
+            prob *= d
+    log = math.log10(prob)
+    prob_list.append(log)
+
+for el in prob_list:
+    print(round(el, 3))
+
+
